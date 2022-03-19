1f7952b1f8a805c4b92a7811a861af4d360cb59b
Vlad Badoiu <vlad_andrei.badoiu@upb.ro>
2022-03-19 13:36:51 +0200

    lab4: Update the skeleton to showcase network order

--- lab4/router.c
+++ lab4/router.c
@@ -55,6 +55,11 @@ int main(int argc, char *argv[])
 		 * the next header is at m.payload + sizeof(struct ether_header) */
 		struct ether_header *eth_hdr = (struct ether_header *) m.payload;
 
+		/* We check if the packet is IPV4. Watch out for the usage of ntohs, why do we need it? Hint: Network Order */
+		if (ntohs(eth_hdr->ether_type) == ETHERTYPE_IP) {
+			
+		}
+
 		/* TODO 3: Check if this is an IPv4 or IPv6 packet and route accordingly. For now we will drop IPv6 packets and forward only IPv4.*/
 
 		/* TODO 4: Check the checksum as required by IPv4  */
