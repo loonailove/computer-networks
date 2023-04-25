a0736f8b589d9b584d8cd55d52895e8e3fec2e2b
Ion-Dorinel FILIP (25005) <dorinel.filip@upb.ro>
2023-04-25 21:48:28 +0000

    Small improvements in plot style

--- lab8/run_client.py
+++ lab8/run_client.py
@@ -36,13 +36,18 @@ def update_plot(frame, cwnd_values, throughput_values):
         ax1.set_xlabel('Time (0.25s intervals)')
         ax1.set_ylabel('Congestion Window Size (cwnd)')
         ax1.set_title('Congestion Window Size vs. Time')
+        ax1.grid(visible=True, axis='y', linestyle=':')
+        ax1.set_ylim(bottom=0)
 
         ax2.clear()
         ax2.plot(throughput_values)
         ax2.set_xlabel('Time (0.25s intervals)')
         ax2.set_ylabel('Throughput (Mbps)')
         ax2.set_title('Throughput vs. Time')
+        ax2.set_ylim(bottom=0)
+        ax2.grid(visible=True, axis='y', linestyle=':')
         ax2.yaxis.set_major_locator(ticker.MultipleLocator(2))
+        ax2.yaxis.set_minor_locator(ticker.MultipleLocator(1))
 
 if __name__ == "__main__":
     target_ip = "172.16.0.100"
